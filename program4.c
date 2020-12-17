43ffff2b786f8c78a97e713c0a8c809096abe52b
Nagaraj U <nagarajullegaddi01@gmail.com>
2020-12-18 00:18:49 +0530

    Update program4.c

--- program4.c
+++ program4.c
@@ -5,6 +5,85 @@ graph using Dijkstra’s shortest path algorithm.
 
 */
 
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+int n;
+
+int findMinVertex(bool visited[n],int distance[n],int n){
+    int minVertex = -1;
+    for(int i=0;i<n;i++){
+        if(!visited[i] && ((distance[minVertex] > distance[i]) || minVertex == -1)){
+            minVertex = i;
+        }
+    }
+    return minVertex;
+}
+
+void dij(int cost[][n],int n)
+{
+    bool visited[n];
+    int distance[n];
+    for(int i=0;i<n;i++){
+        visited[i] = false;
+        distance[i] = 9999;
+    }
+    distance[0] = 0;
+    for(int i=0;i<n-1;i++){
+        int minVertex = findMinVertex(visited,distance,n);
+        visited[minVertex] = true;
+        for(int j=0;j<n;j++){
+            if(!visited[j] && cost[minVertex][j] != 0){
+                int currDistance = distance[minVertex] + cost[minVertex][j];
+                if(currDistance < distance[j]){
+                    distance[j] = currDistance;
+                }
+            }
+        }
+    }
+
+    for(int i=0;i<n;i++){
+        printf("%d \t",i);
+        printf("%d \t",distance[i]);
+        printf("\n");
+    }
+    return;
+}
+
+int main()
+{
+    
+    scanf("%d",&n);
+    int cost[n][n];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            scanf("%d",&cost[i][j]);
+        }
+    }
+    dij(cost,n);
+    return 0;
+   
+}
+
+
+/*
+5
+0 10 0 30 100
+10 0 50 0 0 
+0 50 0 20 10
+30 0 20 0 60
+100 0 10 60 0
+Node    Dist
+0       0       
+1       10
+2       50
+3       30
+4       60
+*/
+
+
+/*
 #include <stdio.h>
 #define infinity 999
 void dij(int n,int v,int cost[10][10],int dist[])
@@ -48,3 +127,4 @@ for(i=1;i<=n;i++)
 if(i!=v)
 printf("%d->%d,cost=%d\n",v,i,dist[i]);
 }
+*/
