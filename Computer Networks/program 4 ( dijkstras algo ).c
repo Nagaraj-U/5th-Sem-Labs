40ddfe15ae8027417a7f36a46c3dd40a1f53344f
Nagaraj U <nagarajullegaddi01@gmail.com>
2020-12-29 21:53:25 +0530

    Update program 4 ( dijkstras algo ).c

--- Computer Networks/program 4 ( dijkstras algo ).c
+++ Computer Networks/program 4 ( dijkstras algo ).c
@@ -3,7 +3,7 @@ Program 4: Given a graph find shortest paths from source to all nodes in the
 graph using Dijkstra’s shortest path algorithm.
 */
 
-
+/*
 #include<stdio.h>
 #include<stdlib.h>
 #include<stdbool.h>
@@ -83,4 +83,95 @@ Node   Dist
 2       50
 3       40      
 4       60
+*/
+*/
+    
+    
+    #include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int getMinVertex(int* distance,bool* visited,int n){
+    int minVertex = -1;
+    for(int i=0;i<n;i++){  //getting min vertex from remaining unvisited nodes which has less distance value
+        if(!visited[i] && (minVertex == -1 || distance[i] < distance[minVertex])){
+            minVertex = i;
+        }
+    }
+    
+    return minVertex;
+}
+
+void dijkstras(int** edges,int n,int source){
+    bool* visited = new bool[n];
+    int* distance = new int[n];
+    for(int i=0;i<n;i++){
+        visited[i] = false;
+        distance[i] = INT_MAX;
+    }
+    distance[source] = 0;
+    
+    for(int i=0;i<n-1;i++){
+        int minVertex = getMinVertex(distance,visited,n);
+        visited[minVertex] = true;
+        
+        for(int j=0;j<n;j++){
+            if(!visited[j] && edges[minVertex][j] != 0){
+                if(distance[j] > (distance[minVertex] + edges[minVertex][j])){ //better way exist to reach 'j' node from minVertex
+                    distance[j] = (distance[minVertex] + edges[minVertex][j]);
+                }
+            }
+        }
+    }
+    
+    //distance array contains distance of each node from source i.e '0th vertex'
+    for(int i=0;i<n;i++){
+        cout << i <<  " " << distance[i] << endl;
+    }
+    
+    return;
+}
+
+int main()
+{
+  
+    int n,e;
+    cout << "enter number of vertices and edges" << endl;
+
+    cin >> n >> e;
+    int** edges = new int*[n]();
+    for(int i=0;i<n;i++){
+        edges[i] = new int[n];
+    }
+    cout << "enter start end weight for all edges"<< endl;
+    for(int i=0;i<e;i++){
+        int start,end,w;
+        cin >> start >> end >> w;
+        edges[start][end] = w;
+        edges[end][start] = w;
+    }
+    int source ;
+    cout << "enter source" << endl;
+    cin >> source;
+    dijkstras(edges,n,source);
+  return 0;
+}
+
+/*
+enter number of vertices and edges
+4 6
+enter start end weight for all edges
+0 1 2
+0 3 3
+0 2 1
+1 2 3
+1 3 4
+2 3 3
+enter source
+1
+0 2
+1 0
+2 3
+3 4
+
 */
