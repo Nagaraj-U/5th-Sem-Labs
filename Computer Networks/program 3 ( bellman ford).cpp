f1bfe681bc6881465e2feb4eb3476cd14f6e73c7
Nagaraj U <nagarajullegaddi01@gmail.com>
2021-01-07 19:54:01 +0530

    Update program 3 ( bellman ford).cpp

--- Computer Networks/program 3 ( bellman ford).cpp
+++ Computer Networks/program 3 ( bellman ford).cpp
@@ -57,3 +57,49 @@ int main()
     bellman(arr,n,e,src);
     return 0;
 } 
+/*
+OUTPUT
+graph : https://media.geeksforgeeks.org/wp-content/uploads/bellmanford3.png
+
+
+enter number of vertices and edges
+5 7
+0 1 6
+0 2 5
+1 3 -1
+2 1 -2
+2 3 4
+2 4 3
+3 4 3
+enter source
+0
+vertex ditance from source
+0 to 0  0
+0 to 1  3
+0 to 2  5
+0 to 3  2
+0 to 4  5
+
+
+
+
+
+enter number of vertices and edges
+5 7
+0 1 6
+0 2 5
+1 3 -1
+2 1 -2
+2 3 4
+2 4 3
+3 4 3
+enter source
+2
+vertex ditance from source
+2 to 0  2147483647
+2 to 1  -2
+2 to 2  0
+2 to 3  -3
+2 to 4  0
+
+*/
